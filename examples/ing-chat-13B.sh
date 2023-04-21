158a4c0194c2c6a38d4543a49e7e3cf4c8b8c7a3
Matthijs van den Bos <matthijs@vandenbos.org>
2023-04-21 11:20:09 +0200

    WIP

--- examples/ing-chat-13B.sh
+++ examples/ing-chat-13B.sh
@@ -2,7 +2,8 @@
 
 cd "$(dirname "$0")/.." || exit
 
-MODEL="${MODEL:-./models/13B/ggml-model-q4_0.bin}"
+MODEL="${MODEL:-./models/7B/ggml-model-q4_0.bin}"
+#MODEL="${MODEL:-./models/13B/ggml-model-q4_0.bin}"
 #MODEL="${MODEL:-./models/ggml-alpaca-7b-q4.bin}"
 USER_NAME="${USER_NAME:-User}"
 PROMPT="${PROMPT:-./prompts/ing-chat-prompt.txt}"
@@ -15,7 +16,7 @@ N_PREDICTS="${N_PREDICTS:-2048}"
 # Note: you can also override the generation options by specifying them on the command line:
 # For example, override the context size by doing: ./chatLLaMa --ctx_size 1024
 #GEN_OPTIONS="${GEN_OPTIONS:---ctx_size 2048 --temp 0.7 --top_k 40 --top_p 0.5 --repeat_last_n 256 --batch_size 1024 --repeat_penalty 1.17647}"
-GEN_OPTIONS="${GEN_OPTIONS:---ctx_size 2048 --temp 0.2 --top_k 10000 --top_p 0.5 --repeat_last_n 256 --batch_size 1024 --repeat_penalty 1}"
+GEN_OPTIONS="${GEN_OPTIONS:---ctx_size 2048 --temp 0.2 --top_k 40 --top_p 0.5 --batch_size 1024 --repeat_penalty 1}"
 
 # shellcheck disable=SC2086 # Intended splitting of GEN_OPTIONS
 ./main $GEN_OPTIONS \
